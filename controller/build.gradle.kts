4b47b27f25dc478c7c587c513ec43b4c00f2acf3
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-12-01 22:37:04 +0900

    [fix] gradleを整理

--- controller/build.gradle.kts
+++ controller/build.gradle.kts
@@ -1,10 +1,9 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
+val ktorVersion: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
 
 plugins {
     application
-    kotlin("jvm") version "1.6.0"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.6.0"
 }
 
@@ -14,15 +13,11 @@ application {
     mainClass.set("io.ktor.server.netty.EngineMain")
 }
 
-repositories {
-    mavenCentral()
-}
-
 dependencies {
-    implementation("io.ktor:ktor-server-core:$ktor_version")
-    implementation("io.ktor:ktor-serialization:$ktor_version")
-    implementation("io.ktor:ktor-server-netty:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-serialization:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    testImplementation("io.ktor:ktor-server-tests:$ktorVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
 }
--- repository/build.gradle.kts
+++ repository/build.gradle.kts
@@ -1,11 +1,3 @@
-plugins {
-    kotlin("jvm") version "1.6.0"
-}
-
-repositories {
-    mavenCentral()
-}
-
 dependencies {
 
 }
