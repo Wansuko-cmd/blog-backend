ea53676c3a5fc470c7499f49ee89c37576018156
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-12-11 21:17:03 +0900

    [delete] 結果を表示するところの削除

--- repositories/article/src/test/kotlin/target/article_repository_impl/get/ArticleRepositoryImplGetTest.kt
+++ repositories/article/src/test/kotlin/target/article_repository_impl/get/ArticleRepositoryImplGetTest.kt
@@ -20,7 +20,7 @@ class ArticleRepositoryImplGetTest {
     @Test
     fun getAllで全てのArticleを取得する() = runBlocking {
         val articles = articleRepository.getAll()
-        assertEquals(TestData, articles)
+        assertEquals(listOf(), articles)
     }
 
     @Test
